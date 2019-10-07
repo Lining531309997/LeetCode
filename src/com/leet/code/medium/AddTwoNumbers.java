4a1a480dd078fc54b22aa459a4e90090fb5ca04a
e <lining531309997@gmail.com>
2019-10-07 21:23:58 +0800

    add two numbers

---
+++ src/com/leet/code/medium/AddTwoNumbers.java
@@ -0,0 +1,7 @@
+package com.leet.code.medium;
+
+/**
+ * Created by lining on 2019/10/7.
+ */
+public class AddTwoNumbers {
+}
---
+++ src/com/leet/code/medium/ListNode.java
@@ -0,0 +1,10 @@
+package com.leet.code.medium;
+
+/**
+ * Created by lining on 2019/10/7.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
