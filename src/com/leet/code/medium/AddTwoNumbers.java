179b202e3785e0605f92c2b5b5407c11be9a083d
e <lining531309997@gmail.com>
2019-10-07 21:29:27 +0800

    add two numbers

--- src/com/leet/code/medium/AddTwoNumbers.java
+++ src/com/leet/code/medium/AddTwoNumbers.java
@@ -4,7 +4,7 @@ package com.leet.code.medium;
  * Created by lining on 2019/10/7.
  */
 public class AddTwoNumbers {
-    public ListNode addTwoNumbers(ListNode list1, ListNode list2) {
+    public ListNode solution(ListNode list1, ListNode list2) {
 
         return new ListNode(-1);
     }
